8b3dcd1d8590fe5eda0e6ca807061d87c4e0afdc
caoyuanbaiyang <275553824@qq.com>
2020-02-11 23:40:47 +0800

    Initial commit

---
+++ model/vsc/vsc.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/14 14:46
+# @Author  : TYQ
+# @File    : vsconvert.py
+# @Software: win10 python3
+import os
+import shutil
+import re
+from fnmatch import fnmatchcase as match
+import copy
+
+
+def isContrainSpecialCharacter(string):
+    # fnmatch 支持的模糊匹配通配符
+    special_character = r"*?[]!"
+    for i in special_character:
+        if i in string:
+            return True
+    return False
+
+
+def exclude_files(filename, dir_filename, excludes=[]):  # 是否属于不下载的文件判断
+    # exclude 为具体配置，支持文件名配置及带目录的配置   # exclude 的不下载，跳过本次循环，进入下一循环
+    if filename in excludes or dir_filename in excludes:
+        return True
+
+    # exclude 为模糊配置，配置的话就不下载，跳过本次循环，进入下一循环
+    for exclude in excludes:
+        if match(filename, exclude) or match(dir_filename, exclude):
+            return True
+
+    # 以上情况都不是这返回False
+    return False
+
+
+def is_need_alert(file, cfg_dir, cfg_file, excludes):
+    (dirname, filename) = os.path.split(file)
+    if not isContrainSpecialCharacter(cfg_file):
+        if os.path.getsize(file) < 1024000 and (cfg_file == file or cfg_dir == dirname) and \
+                not exclude_files(filename, file, excludes):
+            rz = True
+        else:
+            rz = False
+    else:
+        # cfg_file中有模糊匹配，需先对cfg_file 做操作
+        (cfg_dirname, cfg_filename) = os.path.split(cfg_file)
+        if os.path.getsize(file) < 1024000 and cfg_dirname == dirname and match(filename, cfg_filename):
+            rz = True
+        else:
+            rz = False
+    return rz
+
+
+class ModelClass(object):
+    def __init__(self, mylog):
+        self.mylog = mylog
+
+    def action(self, hostname, pub_param, param):
+        source_dir = os.path.join(pub_param["source"], hostname)
+        dest_dir = os.path.join(pub_param["dest"], hostname)
+
+        if not os.path.isdir(dest_dir):
+            os.makedirs(dest_dir)
+
+        if not ('copy_exclude' in param):
+            copy_excludes = []
+        else:
+            copy_excludes = param["copy_exclude"]
+
+        swithparam = copy.deepcopy(param)
+        swithparam.pop('copy_exclude')
+
+        self.dir_copy(source_dir, dest_dir, swithparam, copy_excludes, pub_param, hostname)
+
+    def dir_copy(self, source_dir, dest_dir, switchparam, copy_excludes, pub_param, hostname):
+        for file in os.listdir(source_dir):
+            src_name = os.path.join(source_dir, file)
+            dest_name = os.path.join(dest_dir, file)
+
+            if exclude_files(file, src_name, copy_excludes):
+                self.mylog.info("忽略拷贝:{}".format(src_name))
+                continue
+
+            if os.path.isfile(src_name):
+                self.mylog.info("文件拷贝:{}".format(src_name))
+                shutil.copy(src_name, dest_name)
+                self.alert_file(dest_name, switchparam, pub_param, hostname)
+            else:
+                if not os.path.isdir(dest_name):
+                    self.mylog.info("目录拷贝:{}".format(src_name))
+                    os.makedirs(dest_name)
+                self.dir_copy(src_name, dest_name, switchparam, copy_excludes, pub_param, hostname)
+
+    def alert_file(self, file, switchparam, pub_param, hostname):
+        (dirname, filename) = os.path.split(file)
+        for switchitem in list(switchparam.items()):
+            file_param = switchitem[0]
+            if file_param.endswith("\\"):
+                # file 为目录
+                cfg_dir = os.path.join(pub_param["dest"], hostname, file_param.rstrip("\\"))
+                cfg_file = ''
+            else:
+                # file 为文件
+                cfg_dir = ''
+                cfg_file = os.path.join(pub_param["dest"], hostname, file_param)
+
+            for replace_item in switchitem[1]:
+                if not ('alert_exclude' in replace_item):
+                    alert_excludes = []
+                else:
+                    alert_excludes = replace_item["alert_exclude"]
+                if is_need_alert(file, cfg_dir, cfg_file, alert_excludes):
+                    self.__alter_file_inner(file, replace_item["regexp"], replace_item["replace"])
+
+    def __alter_file_inner(self, file, pattern, replace):
+        try:
+            with open(file, "r", encoding="utf8", newline="") as f1, open("%s.bak" % file, "w", encoding="utf8", newline="") as f2:
+                for (rownum, line) in enumerate(f1):
+                    line_new = re.sub(pattern, replace, line)
+                    if line != line_new:
+                        self.mylog.info("  匹配行{}, 内容:{}".format(rownum, line.replace("\n", "")))
+                        self.mylog.info("  替换行{}, 内容:{}".format(rownum, line_new.replace("\n", "")))
+                    f2.write(line_new)
+            os.remove(file)
+            os.rename("%s.bak" % file, file)
+        except:
+            with open(file, "r", newline="") as f1, open("%s.bak" % file, "w", newline="") as f2:
+                for (rownum, line) in enumerate(f1):
+                    line_new = re.sub(pattern, replace, line)
+                    if line != line_new:
+                        self.mylog.info("  匹配行{}, 内容:{}".format(rownum, line.replace("\n", "")))
+                        self.mylog.info("  替换行{}, 内容:{}".format(rownum, line_new.replace("\n", "")))
+                    f2.write(line_new)
+            os.remove(file)
+            os.rename("%s.bak" % file, file)
---
+++ vsconvert.py
@@ -0,0 +1,127 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2020/1/10 16:08
+# @Author  : TYQ
+# @File    : vsconvert.py
+# @Software: win10 python3
+import logging
+import shutil
+
+from lib.readcfg import ReadCfg
+from lib.Logger import logger
+import os
+import importlib
+
+log_file = "syslog.log"
+groups_file = "config/groups.yaml"
+action_file = "config/action.yaml"
+
+
+class VsConvert(object):
+    def __init__(self, filepath=None):
+        self.mylog = logger(log_file, logging.INFO, logging.INFO)
+        if os.path.exists(groups_file):
+            self.groups = ReadCfg().readcfg(groups_file)
+        else:
+            self.groups = {}
+        if filepath is None:
+            self.action_cfg = ReadCfg().readcfg(action_file)
+        else:
+            self.action_cfg = ReadCfg().readcfg(filepath)
+
+    def __get_host_name(self, hostsnames):
+        # hostsnames 可以是列表，可以是ALL，可以是具体主机名
+        # 还可以是组名
+        rt_hostnames = []
+        if isinstance(hostsnames, list):
+            # hosts.yaml 中部分主机 或 groups.yaml中指定的组
+            for host in hostsnames:
+                if host in self.groups:
+                    # host 是组，需要获取组员
+                    if isinstance(self.groups[host], dict):
+                        # 嵌套组
+                        if "children" not in self.groups[host]:
+                            self.mylog.cri("嵌套组配置错误，必须有children关键字")
+                            raise Exception("嵌套组配置错误，必须有children关键字")
+                        for children_group in self.groups[host]["children"]:
+                            rt_hostnames = rt_hostnames + self.groups[children_group]
+                    else:
+                        # 非嵌套组
+                        rt_hostnames = rt_hostnames + self.groups[host]
+                else:
+                    # host 是具体的主机
+                    rt_hostnames.append(host)
+
+        elif isinstance(hostsnames, str):
+            if hostsnames == "ALL":
+                # hosts.yaml 中全部的主机
+                for hostdir in os.listdir(self.action_cfg["PUBLIC"]["source"]):
+                    if os.path.isdir(os.path.join(self.action_cfg["PUBLIC"]["source"], hostdir)):
+                        rt_hostnames.append(hostdir)
+            else:
+                # hosts.yaml 中某一台主机，后续弃用，先保持兼容
+                rt_hostnames.append(hostsnames)
+        else:
+            raise Exception("action文件hosts配置有误")
+        return rt_hostnames
+
+    def __check_hostname(self, hostsnames):
+        # hostnames 为主机名列表，该函数检查主机名是否在hosts.yaml中
+        rt_hostnames = []
+        source_dir = self.action_cfg["PUBLIC"]["source"]
+
+        for hostname in hostsnames:
+            hostname_dir = os.path.join(source_dir, hostname)
+            if not os.path.isdir(hostname_dir):
+                # 检查失败
+                rt_hostnames.append(hostname)
+        return rt_hostnames
+
+    def __check_acton_hostcfg(self):
+        hostnames = []
+        err_hostnames = []
+        for action in self.action_cfg["ACTION"]:
+            hostnames = hostnames + self.__get_host_name(action["hosts"])
+            err_hostnames = err_hostnames + self.__check_hostname(hostnames)
+
+        if len(err_hostnames) != 0:
+            return False, err_hostnames
+        else:
+            return True, []
+
+    def __action_func_inner(self, hostname, modelname, pub_param, param):
+        self.mylog.info("------模块:{mod},主机:{host}" .format(host=hostname, mod=modelname))
+        if not os.path.isdir(pub_param["source"]):
+            self.mylog.cri("public source目录不存在")
+            raise Exception("public source目录不存在")
+
+        m = importlib.import_module("model." + modelname + "." + modelname)
+        m.ModelClass(self.mylog).action(hostname, pub_param, param)
+
+    def action_func(self):
+        # 检查hosts 配置是否有错误的，如果有错误，则不运行
+        checkrz, rzlist = self.__check_acton_hostcfg()
+        if not checkrz:
+            self.mylog.cri(f"action 配置文件中hosts错误：{rzlist}")
+            raise Exception("action 配置文件hosts配置错误")
+
+        # 开始-测试用，正式发布时去掉
+        pub_param = self.action_cfg["PUBLIC"]
+        if os.path.isdir(pub_param["dest"]):
+            shutil.rmtree(pub_param["dest"])
+        os.makedirs(pub_param["dest"])
+        # 结束-测试用，正式发布时去掉
+
+        for action in self.action_cfg["ACTION"]:
+            # 遍历hosts列表
+            hostname_list = self.__get_host_name(action["hosts"])
+            for task in action["tasks"]:
+                # 遍历task列表
+                self.mylog.info('#########执行任务：{task}#########'.format(task=task["name"]))
+                # 模块信息
+                for modelname, param in task.items():
+                    if modelname == "name":
+                        continue
+                    for hostname in hostname_list:
+                        self.__action_func_inner(hostname, modelname, self.action_cfg["PUBLIC"], param)
+        self.mylog.info('#########所有任务完成#########')
