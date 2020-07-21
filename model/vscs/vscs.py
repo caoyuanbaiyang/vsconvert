3d1057d69fe67ed6dcf53ddf9243087cba49c239
caoyuanbaiyang <tangyouquangood@163.com>
2020-07-21 15:24:49 +0800

    增加vscs模块

--- model/vscs/vscs.py
+++ model/vscs/vscs.py
@@ -5,8 +5,7 @@
 # @Author  : TYQ
 # @File    : vsconvert.py
 # @Software: win10 python3
-
-
+import codecs
 import os
 import shutil
 import re
@@ -41,23 +40,30 @@ def exclude_files(filename, dir_filename, excludes=[]):  # 是否属于不下载
     return False
 
 
-def is_need_alert(file, cfg_dir, cfg_file, excludes):
+def is_need_alert(file, excludes):
     (dirname, filename) = os.path.split(file)
-    if not isContrainSpecialCharacter(cfg_file):
-        if os.path.getsize(file) < 1024000 and (cfg_file == file or cfg_dir == dirname) and \
-                not exclude_files(filename, file, excludes):
-            rz = True
-        else:
-            rz = False
+    if os.path.getsize(file) < 1024000 and not exclude_files(dirname, filename, excludes):
+        rz = True
     else:
-        # cfg_file中有模糊匹配，需先对cfg_file 做操作
-        (cfg_dirname, cfg_filename) = os.path.split(cfg_file)
-        if os.path.getsize(file) < 1024000 and cfg_dirname == dirname and match(filename, cfg_filename):
-            rz = True
-        else:
-            rz = False
+        rz = False
     return rz
 
+
+#: BOMs to indicate that a file is a text file even if it contains zero bytes.
+_TEXT_BOMS = (
+    codecs.BOM_UTF16_BE,
+    codecs.BOM_UTF16_LE,
+    codecs.BOM_UTF32_BE,
+    codecs.BOM_UTF32_LE,
+    codecs.BOM_UTF8,
+)
+
+
+def is_binary_file(file_path):
+    with open(file_path, 'rb') as file:
+        initial_bytes = file.read(8192)
+        file.close()
+    return not any(initial_bytes.startswith(bom) for bom in _TEXT_BOMS) and b'\0' in initial_bytes
 
 class ModelClass(object):
     def __init__(self, mylog):
@@ -71,11 +77,11 @@ class ModelClass(object):
             if "y" == input(f"目录{dest_dir}已存在，删除才可继续，\n 请确认是否删除(y/n)？"):
                 shutil.rmtree(dest_dir)
             else:
-                self.mylog.cri(f"目录{dest_dir}以存在，选择推出")
+                self.mylog.cri(f"目录{dest_dir}以存在，选择退出")
                 exit
-        # 文件夹拷贝
+        # 文件夹拷贝，主机名级别
         shutil.copytree(source_dir, dest_dir)
-        self.mylog.info(f"目录{source_dir}拷贝结束，拷贝目的{dest_dir}")
+        self.mylog.info(f"目录{source_dir}拷贝结束，\n 拷贝目的{dest_dir}")
 
         # 读取配置信息
         for i, param_item in enumerate(param, start=1):
@@ -84,64 +90,64 @@ class ModelClass(object):
                 dirs = param_item["dirs"]
             # rpls 替换语法配置
             if "rpls" in param_item:
+                # 循环目录进行处理
                 for dir_item in dirs:
-                    rpl_dir = list(dir_item)[0]
-                    rec_identify = list(dir_item)[1]
-                    if rpl_dir.endswith("\\") or rpl_dir == "$HOME":
-                        if rpl_dir == "$HOME":
-                            rpl_dir = ""
-                        # 循环读取rpl_dir 下面的文件或文件夹
-                        for file in os.listdir(os.path.join(dest_dir, rpl_dir)):
-                            path_file = os.path.join(dest_dir, rpl_dir, file)
-                            # 如果是文件
-                            if not os.path.isdir(path_file):
-                                pass
-                                # todo
-                            if os.path.isdir(path_file) and rec_identify == "+r":
-                                pass
-                                # todo
-                    else:
-                        if isContrainSpecialCharacter(rpl_dir):
-                            pass
-                        else:
-                            pass
+                    rpl_dir = list(dir_item.keys())[0]
+                    rec_identify = list(dir_item.values())[0]
+                    self.alert_dir_file(rpl_dir, rec_identify, dest_dir, param_item["rpls"])
 
-                    for replace_item in param_item:
-                        # replace_item["regexp"]
-                        # replace_item["replace"]
-                        # replace_item["alert_exclude"]
-                        pass
+    def alert_dir_file(self, rpl_dir, rec_identify, dest_dir, switchparam, filename_patten="*"):
+        # rpl_dir 主机下面的目录
+        # rec_identify 是否目录循环
+        # dest_dir   主机级别的绝对路径
+        # switchparam  rpls下面的配置信息
+        # filename_patten 配置的文件名模糊匹配
 
-    def alert_file(self, file, switchparam, pub_param, hostname):
-        (dirname, filename) = os.path.split(file)
-        for switchitem in list(switchparam.items()):
-            file_param = switchitem[0]
-            if file_param.endswith("\\"):
-                # file 为目录
-                cfg_dir = os.path.join(pub_param["dest"], hostname, file_param.rstrip("\\"))
-                cfg_file = ''
+        # dirs 配置项为文件夹
+        if rpl_dir.endswith("\\") or rpl_dir == "$HOME":
+            if rpl_dir == "$HOME":
+                rpl_dir = ""
+            # 循环读取rpl_dir 下面的文件或文件夹
+            if os.path.exists(os.path.join(dest_dir, rpl_dir)):
+                for file in os.listdir(os.path.join(dest_dir, rpl_dir)):
+                    path_file = os.path.join(dest_dir, rpl_dir, file)
+                    # 如果是文件
+                    if not os.path.isdir(path_file) and match(file, filename_patten):
+                        self.alert_file(path_file, switchparam)
+                    # 如果是目录且循环
+                    if os.path.isdir(path_file) and rec_identify == "+r":
+                        self.alert_dir_file(os.path.join(rpl_dir, file) + "\\", rec_identify, dest_dir, switchparam)
+
+        # dirs 配置项为文件
+        else:
+            if isContrainSpecialCharacter(rpl_dir):
+                (dirname, filename) = os.path.split(rpl_dir)
+                self.alert_dir_file(dirname + "\\", "-r", dest_dir)
             else:
-                # file 为文件
-                cfg_dir = ''
-                cfg_file = os.path.join(pub_param["dest"], hostname, file_param)
+                self.alert_file(os.path.join(dest_dir, rpl_dir), switchparam)
 
-            for replace_item in switchitem[1]:
-                if not ('alert_exclude' in replace_item):
-                    alert_excludes = []
-                else:
-                    alert_excludes = replace_item["alert_exclude"]
-                if is_need_alert(file, cfg_dir, cfg_file, alert_excludes):
-                    # replace 可支持lib,如随机密码函数randpwd
-                    self.__alter_file_inner(file, replace_item["regexp"], replace_item["replace"])
+    def alert_file(self, file, switchparam):
+        # 如果file 是二进制文件则跳过
+        if is_binary_file(file):
+            return
+        for switch_item in switchparam:
+            if not ('alert_exclude' in switch_item):
+                alert_excludes = []
+            else:
+                alert_excludes = switch_item["alert_exclude"]
+            if is_need_alert(file, alert_excludes):
+                # replace 可支持lib,如随机密码函数randpwd
+                self.__alter_file_inner(file, switch_item["regexp"], switch_item["replace"])
 
     def __alter_file_inner(self, file, pattern, replace):
+        self.mylog.info(f"文件：{file} 查找:{pattern} ")
         repl_new = replace
         rem = re.match(REPLACE_PATTERN, replace)
 
-        fbytes = min(2048, os.path.getsize(file))
+        fbytes = min(20480, os.path.getsize(file))
         result = chardet.detect(open(file, 'rb').read(fbytes))
         encoding = result['encoding']
-        with open(file, "r", newline="", encoding=encoding) as f1, open("%s.bak" % file, "w", newline="",
+        with open(file, "r", newline="", encoding=encoding) as f1, open("%s.bak.tyq" % file, "w", newline="",
                                                                         encoding=encoding) as f2:
             for (rownum, line) in enumerate(f1, 1):
                 if rem:
@@ -158,4 +164,4 @@ class ModelClass(object):
                     self.mylog.info("  替换行{}, 内容:{}".format(rownum, line_new.replace("\n", "")))
                 f2.write(line_new)
         os.remove(file)
-        os.rename("%s.bak" % file, file)
+        os.rename("%s.bak.tyq" % file, file)
