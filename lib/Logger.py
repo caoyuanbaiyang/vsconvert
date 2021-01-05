89b9fa30f9ce779860a681e444b6915439eb8f29
caoyuanbaiyang <tangyouquangood@163.com>
2021-01-05 17:06:35 +0800

    增加颜色输出

--- lib/Logger.py
+++ lib/Logger.py
@@ -1,5 +1,20 @@
 # coding=utf-8
 import logging
+import ctypes
+
+FOREGROUND_WHITE = 0x0007
+FOREGROUND_BLUE = 0x01  # text color contains blue.
+FOREGROUND_GREEN = 0x02  # text color contains green.
+FOREGROUND_RED = 0x04  # text color contains red.
+FOREGROUND_YELLOW = FOREGROUND_RED | FOREGROUND_GREEN
+
+STD_OUTPUT_HANDLE = -11
+std_out_handle = ctypes.windll.kernel32.GetStdHandle(STD_OUTPUT_HANDLE)
+
+
+def set_color(color, handle=std_out_handle):
+    bools = ctypes.windll.kernel32.SetConsoleTextAttribute(handle, color)
+    return bools
 
 
 class logger:
@@ -18,20 +33,31 @@ class logger:
         self.logger.addHandler(sh)
         self.logger.addHandler(fh)
 
+    def green(self, message, color=FOREGROUND_GREEN):
+        set_color(color)
+        self.logger.info(message)
+        set_color(FOREGROUND_WHITE)
+
     def debug(self, message):
         self.logger.debug(message)
 
     def info(self, message):
         self.logger.info(message)
 
-    def war(self, message):
+    def war(self, message, color=FOREGROUND_YELLOW):
+        set_color(color)
         self.logger.warning(message)
+        set_color(FOREGROUND_WHITE)
 
-    def error(self, message):
+    def error(self, message, color=FOREGROUND_RED):
+        set_color(color)
         self.logger.error(message)
+        set_color(FOREGROUND_WHITE)
 
-    def cri(self, message):
+    def cri(self, message, color=FOREGROUND_RED):
+        set_color(color)
         self.logger.critical(message)
+        set_color(FOREGROUND_WHITE)
 
 
 if __name__ == '__main__':
