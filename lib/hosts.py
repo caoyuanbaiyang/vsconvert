2f78fb45fc17d4a29afe0b9ffe526052dc4ee4b2
caoyuanbaiyang <tangyouquangood@163.com>
2021-05-06 09:22:05 +0800

    为支持action文件host配置中hostname[x:y]的支持

---
+++ lib/hosts.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2021/5/6 7:34
+# @Author  : TYQ
+# @File    : hosts.py.py
+# @Software: win10 python3
+import os
+import re
+
+PATTERN_WITH_SUBSCRIPT = re.compile(
+    r'''^
+        (.+)                    # A pattern expression ending with...
+        \[                    # A [subscript] expression comprising:            
+            ([0-9]+)([:])      # Or an x:y or x: range.
+            ([0-9]+)
+        \]
+        $
+    ''', re.X
+)
+
+
+class hosts(object):
+    def __init__(self, mylog, groups, destdir):
+        self.mylog = mylog
+        self.groups = groups
+        self.hosts = [dirname for dirname in os.listdir(destdir)if os.path.isdir(os.path.join(destdir, dirname))]
+
+    def get_host_name(self, hostsnames):
+        # hostsnames 可以是列表，可以是ALL，可以是具体主机名
+        # 还可以是组名
+        rt_hostnames = []
+        err_rt_hostnames = []
+        if isinstance(hostsnames, list):
+            # hosts.yaml 中部分主机 或 groups.yaml中指定的组
+            for host in hostsnames:
+                tmp_err_hosts = []
+                if host in self.groups:
+                    # host 是组，需要获取组员
+                    if isinstance(self.groups[host], dict):
+                        # 嵌套组
+                        if "children" not in self.groups[host]:
+                            self.mylog.cri("嵌套组配置错误，必须有children关键字")
+                            raise Exception("嵌套组配置错误，必须有children关键字")
+                        for children_group in self.groups[host]["children"]:
+                            # 具体组名下面的hostname 支持正则表达式
+                            for pattern in self.groups[children_group]:
+                                ok_hostnames, err_hostnames = self.get_host_name_from_pattern(pattern)
+                                rt_hostnames = rt_hostnames + ok_hostnames
+                                tmp_err_hosts = tmp_err_hosts + err_hostnames
+                    else:
+                        # 非嵌套组， 具体组名下面的hostname 支持正则表达式
+                        for pattern in self.groups[host]:
+                            ok_hostnames, err_hostnames = self.get_host_name_from_pattern(pattern)
+                            rt_hostnames = rt_hostnames + ok_hostnames
+                            tmp_err_hosts = tmp_err_hosts + err_hostnames
+                else:
+                    # host 是具体的主机，支持正则表达式
+                    ok_hostnames, err_hostnames = self.get_host_name_from_pattern(host)
+                    rt_hostnames = rt_hostnames + ok_hostnames
+                    tmp_err_hosts = tmp_err_hosts + err_hostnames
+                if len(tmp_err_hosts) > 0:
+                    err_rt_hostnames.append(host)
+
+        elif isinstance(hostsnames, str):
+            if hostsnames == "ALL":
+                # hosts.yaml 中全部的主机
+                for hostname in self.hosts:
+                    rt_hostnames.append(hostname)
+            else:
+                raise Exception("action文件hosts配置有误，字符串类hosts只支持ALL,其他需要用[] 表示")
+        else:
+            raise Exception("action文件hosts配置有误，类型只能为ALL或[]")
+        return rt_hostnames, err_rt_hostnames
+
+    def get_host_name_from_pattern(self, pattern):
+        # pattern 可以是正则表达式形式的
+        # group.yaml 中组及action.yaml hosts 支持正则表达式，因此补仓改函数
+        rt_hostnames = []
+        err_hostnames = []
+        m = PATTERN_WITH_SUBSCRIPT.match(pattern)
+
+        # 不含[x:y]字符情况
+        if not m:
+            if pattern in self.hosts:
+                rt_hostnames.append(pattern)
+            else:
+                err_hostnames.append(pattern)
+        else:
+            # 包含[x:y]字符情况
+            (lable, start, sep, end) = m.groups()
+            t_hosts = []
+            for i in range(int(start), int(end) + 1):
+                hostname = f"{lable}{str(i)}"
+                if hostname in self.hosts:
+                    rt_hostnames.append(hostname)
+
+        if len(rt_hostnames) == 0:
+            err_hostnames.append(pattern)
+        return rt_hostnames, err_hostnames
+
+    def check_hostname(self, hostsnames):
+        # hostnames 为主机名列表，该函数检查主机名是否在hosts.yaml中
+        rt_hostnames = []
+        for hostname in hostsnames:
+            if hostname not in self.hosts:
+                # 检查失败，不在hosts.yaml中
+                rt_hostnames.append(hostname)
+        return rt_hostnames
--- vsconvert.py
+++ vsconvert.py
@@ -6,7 +6,7 @@
 # @Software: win10 python3
 import logging
 import shutil
-
+from lib.hosts import hosts
 from lib.readcfg import ReadCfg
 from lib.Logger import logger
 import os
@@ -29,63 +29,15 @@ class VsConvert(object):
         else:
             self.action_cfg = ReadCfg().readcfg(filepath)
 
-    def __get_host_name(self, hostsnames):
-        # hostsnames 可以是列表，可以是ALL，可以是具体主机名
-        # 还可以是组名
-        rt_hostnames = []
-        if isinstance(hostsnames, list):
-            # hosts.yaml 中部分主机 或 groups.yaml中指定的组
-            for host in hostsnames:
-                if host in self.groups:
-                    # host 是组，需要获取组员
-                    if isinstance(self.groups[host], dict):
-                        # 嵌套组
-                        if "children" not in self.groups[host]:
-                            self.mylog.cri("嵌套组配置错误，必须有children关键字")
-                            raise Exception("嵌套组配置错误，必须有children关键字")
-                        for children_group in self.groups[host]["children"]:
-                            rt_hostnames = rt_hostnames + self.groups[children_group]
-                    else:
-                        # 非嵌套组
-                        rt_hostnames = rt_hostnames + self.groups[host]
-                else:
-                    # host 是具体的主机
-                    rt_hostnames.append(host)
-
-        elif isinstance(hostsnames, str):
-            if hostsnames == "ALL":
-                # hosts.yaml 中全部的主机
-                for hostdir in os.listdir(self.action_cfg["PUBLIC"]["source"]):
-                    if os.path.isdir(os.path.join(self.action_cfg["PUBLIC"]["source"], hostdir)):
-                        rt_hostnames.append(hostdir)
-            else:
-                # hosts.yaml 中某一台主机，后续弃用，先保持兼容
-                rt_hostnames.append(hostsnames)
-        else:
-            raise Exception("action文件hosts配置有误")
-        return rt_hostnames
-
-    def __check_hostname(self, hostsnames):
-        # hostnames 为主机名列表，该函数检查主机名是否在hosts.yaml中
-        rt_hostnames = []
-        source_dir = self.action_cfg["PUBLIC"]["source"]
-
-        for hostname in hostsnames:
-            hostname_dir = os.path.join(source_dir, hostname)
-            if not os.path.isdir(hostname_dir):
-                # 检查失败
-                rt_hostnames.append(hostname)
-        return rt_hostnames
-
-    def __check_acton_hostcfg(self):
+    def __check_acton_hostcfg(self, hostobj):
         hostnames = []
-        err_hostnames = []
+        err_rt_hostnames = []
         for action in self.action_cfg["ACTION"]:
-            hostnames = hostnames + self.__get_host_name(action["hosts"])
-            err_hostnames = err_hostnames + self.__check_hostname(hostnames)
-
-        if len(err_hostnames) != 0:
-            return False, err_hostnames
+            ok_hostnames, err_hostnames = hostobj.get_host_name(action["hosts"])
+            hostnames = hostnames + ok_hostnames
+            err_rt_hostnames = err_rt_hostnames + err_hostnames
+        if len(err_rt_hostnames) != 0:
+            return False, err_rt_hostnames
         else:
             return True, []
 
@@ -100,7 +52,8 @@ class VsConvert(object):
 
     def action_func(self):
         # 检查hosts 配置是否有错误的，如果有错误，则不运行
-        checkrz, rzlist = self.__check_acton_hostcfg()
+        hostobj = hosts(self.mylog, self.groups, self.action_cfg["PUBLIC"]["source"])
+        checkrz, rzlist = self.__check_acton_hostcfg(hostobj)
         if not checkrz:
             self.mylog.cri(f"action 配置文件中hosts错误：{rzlist}")
             raise Exception("action 配置文件hosts配置错误")
@@ -116,13 +69,16 @@ class VsConvert(object):
 
         for action in self.action_cfg["ACTION"]:
             # 遍历hosts列表
-            hostname_list = self.__get_host_name(action["hosts"])
+            hostname_list, err_host_list = hostobj.get_host_name(action["hosts"])
             for task in action["tasks"]:
                 # 遍历task列表
                 self.mylog.info('#########执行任务：{task}#########'.format(task=task["name"]))
                 # 模块信息
                 for modelname, param in task.items():
                     if modelname == "name":
+                        continue
+                    if modelname == "GetHostList":
+                        self.mylog.info(f"主机列表：{hostname_list}")
                         continue
                     for hostname in hostname_list:
                         self.__action_func_inner(hostname, modelname, self.action_cfg["PUBLIC"], param)
