9c0e8a1132dbab4afede8f70c6981093eafc14a4
tyq-ext-pc <275553824@qq.com>
2022-08-23 09:09:03 +0800

    增加随机密码lib函数randompwd

--- lib/replace_lib.py
+++ lib/replace_lib.py
@@ -5,6 +5,8 @@
 # @File    : replace_lib.py
 # @Software: win10 python3
 import random
+import string
+
 
 s1 = "abcdefghijklmnopqrstuvwxyz"
 s4 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
@@ -12,7 +14,7 @@ s2 = "1234567890"
 s3 = "!@#$%^&*"
 
 
-def randompwd(param):
+def randompwd1(param):
     pwd = ""
     for i in range(2):
         pwd += s1[random.randint(0, len(s1)-1)]
@@ -24,5 +26,21 @@ def randompwd(param):
     return pwd
 
 
+def randompwd(param):
+    str_long = int(param)
+
+    lowercase_num = random.randint(2, 4)
+    uppercase_num = random.randint(2, str_long - lowercase_num - 3)
+    digits_num = str_long - 2 - uppercase_num - lowercase_num
+
+    password = random.sample(string.ascii_uppercase, uppercase_num) + random.sample(string.digits, digits_num) + random.sample(
+        string.ascii_lowercase, lowercase_num) + list(random.choice(s3))
+
+    random.shuffle(password)
+    pwd = random.choice(string.ascii_lowercase) + ''.join(password)
+    return pwd
+
+
 if __name__ == '__main__':
-    print(randompwd())
+    for i in range(100):
+        print(randompwd(9))
