8aee29e4953447f402e1d9cf8ed66cc1b7ab3208
tyq-ext-pc <275553824@qq.com>
2024-01-16 11:11:53 +0800

    增加数据库模板文件的处理

--- model/tplrender/tplrender.py
+++ model/tplrender/tplrender.py
@@ -6,17 +6,27 @@
 # @Software: win10 python3
 import os
 import shutil
-import fnmatch
 import glob
 import json
+import re
 
-from jinja2 import Environment, TemplateNotFound, BaseLoader, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, Template
+
+C_PATTERN = r'{{db_info\..*\.pwd}}'
 
 
 def load_json(filename):
     with open(filename, 'r') as file:
         data = json.load(file)
     return data
+
+
+def check_file_for_pattern(file_path, pattern):
+    with open(file_path, 'r') as file:
+        for line in file:
+            if re.search(pattern, line):
+                return True
+    return False
 
 
 class ModelClass(object):
@@ -57,8 +67,9 @@ class ModelClass(object):
         render_vars = self.load_vars_files(vars_files)
         # 获取模板文件夹列表
         template_dirs = self.get_template_dirs(param["templates"])
+        template_files = self.get_template_files(template_dirs)
         #  渲染模板文件
-        self.render_template(template_dirs, render_vars)
+        self.render_template_files(template_files, render_vars)
 
     def get_vars_files(self, vars_files_cfgs: list) -> list:
         vars_files = []
@@ -92,23 +103,68 @@ class ModelClass(object):
                     template_dirs.append(os.path.join(self.dest_dir, directory))
         return template_dirs
 
+    def get_template_files(self, directory_list: list) -> list:
+        tpl_files = []
+        for directory in directory_list:
+            for root, dirs, files in os.walk(directory):
+                for file in files:
+                    if os.path.splitext(file)[1] not in self.template_suffix:
+                        self.mylog.debug(f"{file} is not a template file")
+                        continue
+                    tpl_files.append(os.path.join(root, file))
+        return tpl_files
+
     # 渲染模板文件
     def render_template(self, template_dirs: list, vars_data: list):
         loader = FileSystemLoader(template_dirs)
         env = Environment(loader=loader, **self.env_cfg)
         template_list = loader.list_templates()
         for template_file in template_list:
-            template_path = env.loader.get_source(env, template_file)[1]
-            if os.path.splitext(template_file)[1] not in self.template_suffix:
-                self.mylog.info(f'  {template_path} is not a template file')
-                continue
-            template = env.get_template(template_file)
-            self.mylog.info(f'  render file: {template_path}')
-            rendered_data = template.render(**vars_data)
-            rendered_file = os.path.splitext(template_path)[0]
-            with open(rendered_file, 'w', encoding='utf-8') as f:
-                f.write(rendered_data)
+            try:
+                template_path = env.loader.get_source(env, template_file)[1]
+                if os.path.splitext(template_file)[1] not in self.template_suffix:
+                    continue
+                template = env.get_template(template_file)
+                self.mylog.info(f'  rendering file: {template_path}')
+                rendered_data = template.render(**vars_data)
+                rendered_file = os.path.splitext(template_path)[0]
+                with open(rendered_file, 'w', encoding='utf-8') as f:
+                    f.write(rendered_data)
 
+                if check_file_for_pattern(template_path, C_PATTERN):
+                    self.mylog.info(f'  {template_path} is a DB template file, need to create a .tmpl file')
+                    with open(template_path, 'r', encoding='utf-8') as f1:
+                        content = f1.read()
+                    content1 = content.replace(r'($${{db_info\..*\.pwd}})', r'{% raw %}\g<1>{% endraw %}')
+                    Template(content1, **self.env_cfg).stream(**vars_data).dump(rendered_file + '.tmpl',
+                                                                                encoding='utf-8')
 
+            except UnicodeDecodeError as e:
+                self.mylog.info(f'  不可打开文件 {template_file} 不转换: {e}')
+            except Exception as e:
+                self.mylog.error(f'  rendering file {template_file} failed: {e}')
 
+    def render_template_files(self, template_files: list, vars_data: list):
+        for template_file in template_files:
+            try:
+                with open(template_file, 'r', encoding='utf-8') as f1:
+                    original_template_string = f1.read()
 
+                self.mylog.info(f'  rendering file: {template_file}')
+                template_all = Template(original_template_string, **self.env_cfg)
+                rendered_data = template_all.render(**vars_data)
+                rendered_file = os.path.splitext(template_file)[0]
+                with open(rendered_file, 'w', encoding='utf-8') as f:
+                    f.write(rendered_data)
+
+                if check_file_for_pattern(template_file, C_PATTERN):
+                    self.mylog.info(f'                DB template file, need to create a .tmpl file')
+                    template_raw_string = re.sub(r'\$\$\{\{db_info\..*?\.pwd\}\}', r'{% raw %}\g<0>{% endraw %}',
+                                                 original_template_string)
+                    Template(template_raw_string, **self.env_cfg).stream(**vars_data).dump(rendered_file + '.tmpl',
+                                                                                           encoding='utf-8')
+
+            except UnicodeDecodeError as e:
+                self.mylog.info(f'  不可打开文件 {template_file} 不转换: {e}')
+            except Exception as e:
+                self.mylog.error(f'  rendering file {template_file} failed: {e}')
